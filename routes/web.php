7afbfe3f96c3179dd8d38f07d0901ff24382bbe7
ketanyekale <ketan.yekale@inscripts.com>
2017-12-01 18:41:18 +0530

    Added route for json response and header and cookie.
    Added route that points to a index function in the Hello.php
    removed public from URL by copying htaccess from public folder to root and renaming server.php to index.php

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,14 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/responseheaderswithrequestparams', function () {
+    return response(json_encode($_REQUEST), 200)
+        	->header('Connection', 'close')
+			->header('cache-control', 'must-revalidate')
+			->header('Vary', 'Accept-Encoding')
+			->header('content-type', 'application/json; charset=utf-8')
+			->cookie('ce1','ketan',1);
+});
+
+Route::get('hello', 'Hello@index');
